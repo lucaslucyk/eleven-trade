037fd6a133e7f18da816156109ceede96e3a8232
Lucas Lucyk <lucaslucyk@gmail.com>
2022-03-29 01:44:49 -0300

    starting project

---
+++ CotpsBot/CotpsBot/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using CotpsBot.Services.Http;
+using CotpsBot.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace CotpsBot
+{
+    public partial class App : Application
+    {
+        #region Fields
+
+        public static RequestService ApiClient;
+
+        #endregion
+        public App()
+        {
+            InitializeComponent();
+
+            ApiClient = new RequestService();
+            DependencyService.RegisterSingleton<Services.Http.IRequestService>(ApiClient);
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
